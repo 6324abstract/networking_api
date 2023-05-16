1c859dbc151de16ce82ad2951a45729dee97a74d
Yanzhe Li <41003148+6324abstract@users.noreply.github.com>
2023-05-16 16:40:30 +0900

    modify test_servers

--- socket_server/test/functional/test_tcp_udp_server_unit.py
+++ socket_server/test/functional/test_tcp_udp_server_unit.py
@@ -2,12 +2,13 @@ from socket_server.src.my_server import *
 import socket
 import threading
 import unittest
+import xmlrpc.client
 
 
 class Test_tcp_udp_server(unittest.TestCase):
     def setUp(self) -> None:
         self.test_server = Server()
-        self.host = '127.0.0.1'
+        self.host = "localhost"
         self.tcp_port, self.udp_port = 12000, 12001
         self.client_message = "test_message"
         self.server_thread_tcp = MyThread(
@@ -27,9 +28,7 @@ class Test_tcp_udp_server(unittest.TestCase):
             sock.connect((self.host, self.udp_port))
             sock.sendall(bytes(self.client_message + "\n", "utf-8"))
             received = str(sock.recv(1024), "utf-8")
-            print(received)
             assert received == self.client_message.upper()
-        sock.close()
 
     def test_tcp_server(self):
         self.server_thread_tcp.start()
@@ -49,12 +48,31 @@ class Test_tcp_udp_server(unittest.TestCase):
 
 
 class Test_rpc_server(unittest.TestCase):
-    pass
+    def setUp(self) -> None:
+        self.host = "localhost"
+        self.rpc_port = 12002
+        self.url = "http://" + self.host + ":" + str(self.rpc_port) + "/RPC2"
+        print(self.url)
+        self.test_server = Server()
+        self.server_thread_rpc = MyThread(
+            target=self.test_server.start_xmlrpc, args=(self.rpc_port,)
+        )
+
+    def test_correct_result(self):
+        self.server_thread_rpc.start()
+        with xmlrpc.client.ServerProxy(self.url) as proxy:
+            assert proxy.add(1, 2) == 3
+
+    def test_unknown_function(self):
+        pass
+
+    def tearDown(self) -> None:
+        self.server_thread_rpc.stop()
 
 
 class MyThread(threading.Thread):
     def __init__(self, target=None, args=()):
-        super().__init__()
+        super().__init__(target=target, args=args)
         self._stop_flag = threading.Event()
 
     def stop(self):
